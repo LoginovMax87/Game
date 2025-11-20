da9f4faba2365e7599cb3951d214eb51109d4a74
Взрослая академия <eduloginov@gmail.com>
2025-11-20 18:38:48 +0300

    Первая версия

--- Person.py
+++ Person.py
@@ -1,28 +1,10 @@
 from Section import Section
 class Person:
-    __section=set()
-
-    def add_section(self, value):
-        if isinstance(value, Section):
-            self.__section.add(value)
-
-    def del_section(self, section_name):
-        for i in self.__section:
-            if i.name == section_name:
-                self.__section.discard(name)
-                break
-
+    __sections = set()
 
     def __init__(self, fio, phone):
         self.__fio = fio
         self.__phone = phone
-
-    def __str__(self):
-        if self.__section:
-            return (f"fio: {self.__fio} | phone: {self.__phone} | {self.__section}")
-        else:
-            return (f"fio: {self.__fio} | phone: {self.__phone}")
-
 
     @property
     def fio(self):
@@ -40,12 +22,29 @@ class Person:
     def phone(self, value):
         self.__phone = value
 
+    def add_section(self, value):
+        if isinstance(value, Section):
+            self.__sections.add(value)
+
+    def del_section(self, section_name):
+        for i in self.__sections:
+            if i.name == section_name:
+                self.__sections.discard(i)
+                break
+
+    def __str__(self):
+        result = ""
+        for i in self.__sections:
+            result += str(i) + "\n"
+        return f"{self.__fio} - {self.__phone} \n{result}"
+
 
 if __name__ == '__main__':
-    person = Person("fio", "5232352352")
-    person.phone=41241241
-    box = Section("Бокс", 25)
-    print(box)
-    person.add_section(box)
+    person = Person("fio", "12563")
+    section = Section("бокс", 23)
+    section2 = Section("гребля", 23)
+    person.add_section(section)
+    person.add_section(section2)
+    person.del_section("бокс")
     print(person)
 
--- Section.py
+++ Section.py
@@ -1,29 +1,11 @@
 class Section:
-
-    def __init__(self, name, quantity):
+    def __init__(self, name, max_count):
         self.__name = name
-        self.__quantity = quantity
+        self.__max_count = max_count
 
     @property
     def name(self):
         return self.__name
 
-    @name.setter
-    def name(self, value):
-        self.__name = value
-
-    @property
-    def quantity(self):
-        return self.__quantity
-
-    @quantity.setter
-    def quantity(self, value):
-        self.__quantity = value
-
     def __str__(self):
-        return (f"name: {self.__name} | quantity: {self.__quantity}")
-
-
-if __name__ == '__main__':
-    box = Section("Бокс", 25)
-    print(box)
+        return f"{self.__name} | {self.__max_count}"
